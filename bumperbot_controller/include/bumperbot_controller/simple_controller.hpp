74b2bce5c05f95076b67624c020f40be7e563848
Jasser000 <zaagjasser@gmail.com>
2024-07-16 23:41:43 -0300

    Calculate and publish wheel odometry in simple_controller
    - Updated CMakeLists.txt for bumperbot_controller
    - Modified simple_controller.hpp to include wheel odometry calculations
    - Updated package.xml with necessary dependencies
    - Modified simple_controller.cpp to calculate wheel odometry, publish odometry data, and broadcast odom frame

--- bumperbot_controller/include/bumperbot_controller/simple_controller.hpp
+++ bumperbot_controller/include/bumperbot_controller/simple_controller.hpp
@@ -6,8 +6,10 @@
 #include <geometry_msgs/msg/twist_stamped.hpp>
 #include <std_msgs/msg/float64_multi_array.hpp>
 #include <sensor_msgs/msg/joint_state.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 #include <Eigen/Core>
-
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/msg/transform_stamped.hpp>
 
 class SimpleController : public rclcpp::Node
 {
@@ -22,6 +24,7 @@ private:
     rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr vel_sub_;
     rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr wheel_cmd_pub_;
     rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_sub_;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
 
     double wheel_radius_;
     double wheel_separation_;
@@ -31,7 +34,14 @@ private:
     double right_wheel_prev_pos_;
     rclcpp::Time prev_time_;
 
+    double x_;
+    double y_;
+    double theta_;
 
+    nav_msgs::msg::Odometry odom_msg_;
+
+    std::unique_ptr<tf2_ros::TransformBroadcaster> transform_broadcaster_;
+    geometry_msgs::msg::TransformStamped transform_stamped_;
 
 };
 
